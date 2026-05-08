0c7cd5d8382e8474ec2d5c153f26777857209ad3
Tanya <yakovetsta1@gmail.com>
2026-05-08 15:13:51 -0300

    add mongodb notes

---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,15 @@
+import { HttpError } from 'http-errors';
+
+export const errorHandler = (err, req, res, next) => {
+  console.error(err);
+
+  if (err instanceof HttpError) {
+    return res.status(err.status).json({
+      message: err.message,
+    });
+  }
+
+  res.status(500).json({
+    message: err.message || 'Something went wrong',
+  });
+};
