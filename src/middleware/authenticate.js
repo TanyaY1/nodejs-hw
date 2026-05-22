804782935f5a9cc18ab849ebc0a3862eaf2dc455
Tanya <yakovetsta1@gmail.com>
2026-05-22 13:57:03 -0300

    fixed sessions id check

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -4,13 +4,14 @@ import { Session } from '../models/session.js';
 import { User } from '../models/user.js';
 
 export const authenticate = async (req, res, next) => {
-  const { accessToken } = req.cookies;
+  const { sessionId, accessToken } = req.cookies;
 
-  if (!accessToken) {
-    throw createHttpError(401, 'Missing access token');
+  if (!sessionId || !accessToken) {
+    throw createHttpError(401, 'Missing session credentials');
   }
 
   const session = await Session.findOne({
+    _id: sessionId,
     accessToken,
   });
 
@@ -18,8 +19,7 @@ export const authenticate = async (req, res, next) => {
     throw createHttpError(401, 'Session not found');
   }
 
-  const isAccessTokenExpired =
-    new Date() > new Date(session.accessTokenValidUntil);
+  const isAccessTokenExpired = session.accessTokenValidUntil < new Date();
 
   if (isAccessTokenExpired) {
     throw createHttpError(401, 'Access token expired');
